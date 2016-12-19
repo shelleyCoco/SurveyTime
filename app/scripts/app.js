5bb3c0068e791eb21649e40512677fae841267bd
coco <215077830@qq.com>
2016-12-19 14:43:52 +0800

    第一天，搭建项目，写静态页面

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name surveyTimeApp
+ * @description
+ * # surveyTimeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('surveyTimeApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name surveyTimeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the surveyTimeApp
+ */
+angular.module('surveyTimeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
