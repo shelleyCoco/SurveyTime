ea7b72661898e84e2e886558e75efd964bc1300f
caodongsheng <281135332@qq.com>
2016-12-20 08:48:58 +0800

    cds1

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,5 +8,23 @@
  *
  * Main module of the application.
  */
+window.onload = function(){
+	document.documentElement.style.fontSize  = innerWidth/16 + 'px';
+	window.onresize = function(){
+		document.documentElement.style.fontSize  = innerWidth/16 + 'px';
+	}
+}
+
 angular
-  .module('surveyTimeApp', []);
+  .module('surveyTimeApp', ['ui.router']).controller('ctrl',['$scope',function($scope){
+	  
+	}]).config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider){
+		$stateProvider.state('cds',{
+			url:'/cds',
+			controller:'cdsController',
+			templateUrl:'views/main.html'
+		});
+		$urlRouterProvider.otherwise('/cds')
+	}]).controller('cdsController',['$scope',function($scope){
+		
+	}]);
