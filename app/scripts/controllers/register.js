0a7b143a40e0e4c7991d431e54ec9a95de0fa2ba
wangjiayu <1033394259@qq.com>
2016-12-22 19:56:53 +0800

    19

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -1,4 +1,4 @@
-angular.module('surveyTimeApp').controller('register', ['$scope', '$http', '$filter','$state',function($scope, $http, $filter, $state) {
+angular.module('surveyTimeApp').controller('register', ['$scope', '$http', '$filter','$state','$timeout',function($scope, $http, $filter, $state,$timeout) {
 		var yhm = /^[a-z0-9_]{6,18}$/;
 		var mm = /^[a-zA-Z\d_]{6,}$/;
 		$scope.username = '';
@@ -7,13 +7,28 @@ angular.module('surveyTimeApp').controller('register', ['$scope', '$http', '$fil
 
 		function veri() {
 			if((!yhm.test($scope.username))) {
-				alert('用户名在6-18，请重新输入');
+//				alert('用户名在6-18，请重新输入');
+                wjy_tishi.style.display='block';
+				wjy_tishi.innerHTML='用户名在6-18，请重新输入'
+				$timeout(function(){
+					wjy_tishi.style.display='none';
+				},3000)
 				return false;
 				}else if(!(mm.test($scope.pw))) {
-					alert('密码长度在6位以上，请重新输入');
+//					alert('密码长度在6位以上，请重新输入');
+                wjy_tishi.style.display='block';
+				wjy_tishi.innerHTML='密码长度在6位以上，请重新输入'
+					$timeout(function(){
+					wjy_tishi.style.display='none';
+				},3000)
 					return false;
 				}else if($scope.pw!=$scope.pw1) {
-					alert('两次输入的密码不一样，请重新输入！');
+//					alert('两次输入的密码不一样，请重新输入！');
+                wjy_tishi.style.display='block';
+				wjy_tishi.innerHTML='两次输入的密码不一样，请重新输入！'
+					$timeout(function(){
+					wjy_tishi.style.display='none';
+				},3000)
 					return false;
 				}else{
 					return true;
@@ -29,10 +44,14 @@ angular.module('surveyTimeApp').controller('register', ['$scope', '$http', '$fil
 						'password': $scope.pw
 					}
 				}).then(function() {
-					alert('注册成功！')
+					//alert('注册成功！')
+					wjy_tishi.style='block';
+					wjy_tishi.innerHTML='注册成功！'
 					$state.go('login')
 				}, function() {
-					alert('用户名已经被注册，换个试试！')
+//					alert('用户名已经被注册，换个试试！')
+                    wjy_tishi.style.display='block';
+					wjy_tishi.innerHTML='用户名已有，请重新输入！'
 				})
 			}
 
