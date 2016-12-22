59f2752c2c6a8de64feed39c4d1eb8bc219c7d46
caodongsheng <281135332@qq.com>
2016-12-22 20:50:04 +0800

    v111111

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,92 +7,10 @@
  * # MainCtrl
  * Controller of the surveyTimeApp
  */
+
 angular.module('surveyTimeApp')
   .controller('cdsController', ['$scope','$http','$state',function ($scope,$http,$state){
   	
-  	$scope.json2=[{
-  		"option":[
-	  		{
-	  			"title":"你每天学习几个小时？",
-	  			"opt":[],
-	  			"type":2,
-	  			"oop":"我每天学习8小时"
-	  		},
-	  		{
-	  			"title":"你每天睡觉几小时",
-	  			"opt":[
-		  			{
-		  				"op":"8小时",
-		  				"num":1
-		  			},
-			  		{
-			  			"op":"9小时",
-			  			"num":1
-			  		}
-		  		],
-		  		"type":0,
-		  		"oop":""
-	  		},
-	  		{
-	  			"title":"你每天吃饭用多久",
-	  			"opt":[
-	  			{
-	  				"op":"1小时",
-	  				"num":1
-	  			},
-	  			{
-	  				"op":"2小时",
-	  				"num":1
-	  			}],
-	  			"type":0,
-	  			"oop":""
-	  		}
-  		],
-  		"uid":"56aa508e239548c2",
-  		"title":"你热爱学习吗",
-  		"id":"6c4087e8acf439b7"
-  	},{
-  		"option":[
-	  		{
-	  			"title":"你每天学习几个小时？",
-	  			"opt":[],
-	  			"type":2,
-	  			"oop":"我每天学习8小时"
-	  		},
-	  		{
-	  			"title":"你每天睡觉几小时",
-	  			"opt":[
-		  			{
-		  				"op":"8小时",
-		  				"num":1
-		  			},
-			  		{
-			  			"op":"9小时",
-			  			"num":1
-			  		}
-		  		],
-		  		"type":0,
-		  		"oop":""
-	  		},
-	  		{
-	  			"title":"你每天吃饭用多久",
-	  			"opt":[
-	  			{
-	  				"op":"1小时",
-	  				"num":1
-	  			},
-	  			{
-	  				"op":"2小时",
-	  				"num":1
-	  			}],
-	  			"type":0,
-	  			"oop":""
-	  		}
-  		],
-  		"uid":"56aa508e239548c2",
-  		"title":"你热爱学习吗",
-  		"id":"6c4087e8acf439b7"
-  	}]
   	$scope.json=[];
   	var url='http://47.90.20.200:1602';
     $http({
@@ -125,3 +43,15 @@ angular.module('surveyTimeApp')
     	},function(){})
     }
   }]);
+
+
+
+
+
+
+
+
+
+
+
+
