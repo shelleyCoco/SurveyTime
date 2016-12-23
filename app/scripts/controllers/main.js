2e9b4af4bd674ccdaf57de48a3e2f8900beb8afd
caodongsheng <281135332@qq.com>
2016-12-23 17:12:41 +0800

    12.23   17.12 del

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,19 +10,27 @@
 
 angular.module('surveyTimeApp')
   .controller('cdsController', ['$scope','$http','$state',function ($scope,$http,$state){
-  	
+  	$scope.isshow=true;
+  	$scope.cdschulai=false;
   	$scope.json=[];
   	var url='http://47.90.20.200:1602';
-    $http({
-    	url:url+'/item/',
-  		method:'get',
-  		params:{'uid':localStorage.uid}
-    }).then(function(e){
-		console.log(e)
-		$scope.json=e.data
-    },function(e){
-    	
-    });
+    $scope.gg=function(){
+    	$http({
+	    	url:url+'/item/',
+	  		method:'get',
+	  		params:{'uid':localStorage.uid}
+	    }).then(function(e){
+			console.log(e)
+
+			$scope.json=e.data;
+			for(var i=0;i<$scope.json.length;i++){
+				$scope.json[i].checked=false;
+			}
+	    },function(e){
+	    	
+	    });
+    }
+    $scope.gg();
     $scope.cdstc=function(){
     	
     	$state.go('login')
@@ -41,6 +49,30 @@ angular.module('surveyTimeApp')
     		console.log(e)
     		$scope.json.splice(index,1)
     	},function(){})
+    };
+    
+    $scope.cdsAll=function(){
+    	$scope.isshow=!$scope.isshow;
+    	$scope.cdschulai=true;
+
+    };
+    $scope.cdsDel=function(){
+    	$scope.isshow=!$scope.isshow;
+		for(var i=0;i<$scope.json.length;i++){
+    		if($scope.json[i].checked==true){
+				var id=$scope.json[i].id
+		    	$http({
+		    		url:url+'/item/'+id,
+		    		method:'delete',
+
+		    	}).then(function(e){
+		    		console.log(e)
+		    		$scope.gg();
+		    	},function(){})
+    		}
+		}
+		
+    	$scope.cdschulai=false;
     }
   }]);
 
