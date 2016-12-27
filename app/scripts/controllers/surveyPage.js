39ff2c3350916d7be9e967610a41c789dbfe9697
caodongsheng <281135332@qq.com>
2016-12-27 10:08:25 +0800

    cds xinyemian

--- app/scripts/controllers/surveyPage.js
+++ app/scripts/controllers/surveyPage.js
@@ -1,15 +1,38 @@
 angular.module("surveyTimeApp")
 .constant("serve","http://47.90.20.200:1602")
-.controller('surveyPage',['$scope','$http','serve',function($scope,$http,serve) {
+.controller('surveyPage',['$scope','$http','serve','$stateParams',function($scope,$http,serve,$stateParams){
+   var id=$stateParams.id;
+   $scope.updata={};
+   $scope.wcledata0=[];
+   $scope.wcledata1=[];
+   $scope.wcledata2=[];
+   $scope.wcledata3=[];
    $http({
    	method:'get',
-   	url:serve+'/item/',
+   	url:serve+'/item/'+id,
    	params:{'uid':localStorage.uid}
 
    }).then(function(e){
-   	console.log(e)
    	$scope.updata=e.data;
-   },function(){
-   	
-   })
-	}]);
+      console.log($scope.updata)
+      for(var i=0;i<$scope.updata.option.length;i++){
+         if($scope.updata.option[i].type==0){
+            $scope.wcledata0.push($scope.updata.option[i])
+         }else if($scope.updata.option[i].type==1){
+            $scope.updata.option[i].token='asd'+i;
+            
+            $scope.wcledata1.push($scope.updata.option[i])
+
+         }else if($scope.updata.option[i].type==2){
+            $scope.wcledata2.push($scope.updata.option[i])
+         }else{
+            $scope.wcledata3.push($scope.updata.option[i])
+         }
+
+      }
+      console.log($scope.wcledata1)
+   },function(){})
+   $scope.Fn=function(){
+      
+   }
+	}])
