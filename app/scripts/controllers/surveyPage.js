70d851d560602ed057abf9ee4b15f094613b0696
caodongsheng <281135332@qq.com>
2016-12-28 20:38:07 +0800

    cds    12/28/20:40

--- app/scripts/controllers/surveyPage.js
+++ app/scripts/controllers/surveyPage.js
@@ -1,12 +1,15 @@
 angular.module("surveyTimeApp")
 .constant("serve","http://47.90.20.200:1602")
-.controller('surveyPage',['$scope','$http','serve','$stateParams',function($scope,$http,serve,$stateParams){
+.controller('surveyPage',['$scope','$http','serve','$stateParams','$state',function($scope,$http,serve,$stateParams,$state){
    var id=$stateParams.id;
    $scope.updata={};
+   $scope.hintTitle = '';
+   $scope.hintB = false
    $scope.wcledata0=[];
    $scope.wcledata1=[];
    $scope.wcledata2=[];
    $scope.wcledata3=[];
+   $scope.data=[];
    $http({
    	method:'get',
    	url:serve+'/item/'+id,
@@ -14,9 +17,12 @@ angular.module("surveyTimeApp")
 
    }).then(function(e){
    	$scope.updata=e.data;
-      console.log($scope.updata)
-
+      //$scope.updata1=e.data
+      //console.log($scope.updata)
+      
       for(var i=0;i<$scope.updata.option.length;i++){
+         $scope.data.push(i);
+
          if($scope.updata.option[i].type==0){
             $scope.wcledata0.push($scope.updata.option[i])
          }else if($scope.updata.option[i].type==1){
@@ -38,23 +44,40 @@ angular.module("surveyTimeApp")
          }
 
       }
-
-      console.log($scope.wcledata1)
+      /*清数据*/
+      for(var i=0;i<$scope.wcledata0.length;i++){
+         if($scope.wcledata0[i].oop!=''){
+            $scope.wcledata0[i].oop='';
+         }
+      }
+      for(var i=0;i<$scope.wcledata3.length;i++){
+         if($scope.wcledata3[i].oop!=''){
+            $scope.wcledata3[i].oop='';
+         }
+      }
+      for(var i=0;i<$scope.wcledata1.length;i++){
+         for(var j=0;j<$scope.wcledata1[i].opt.length;j++){
+            $scope.wcledata1[i].opt[j].check=-1;
+         }
+      }
+      /*题号*/
+      $scope.ind0=1;
+      $scope.ind1=$scope.wcledata0.length+$scope.ind0;
+      $scope.ind2=$scope.wcledata1.length+$scope.ind1;
+      $scope.ind3=$scope.wcledata2.length+$scope.ind2;
+      //console.log($scope.wcledata1)
    },function(){})
    $scope.ssss='';
    /*单选*/
 
    $scope.cdsfuzhi=function(parentIndex,index){
-      for(var i=0;i<$scope.wcledata1[parentIndex].opt.length;i++){
-         $scope.wcledata1[parentIndex].opt[i].check=-1;
-      }
+      
       $scope.wcledata1[parentIndex].opt[index].check=1;
       
    }
-   console.log($scope.wcledata1)
    /*多选*/ 
    $scope.ffn=function(parentIndex,index){
-      console.log($scope.wcledata2[parentIndex].opt[index].check)
+      //console.log($scope.wcledata2[parentIndex].opt[index].check)
       if($scope.wcledata2[parentIndex].opt[index].check==1){
          $scope.wcledata2[parentIndex].opt[index].check=2;
          //console.log($scope.wcledata2[parentIndex].opt[index].check)
@@ -69,6 +92,47 @@ angular.module("surveyTimeApp")
    }
    /*提交*/
    $scope.Fn=function(){
+      /*单选验证*/
+      for(var i=0;i<$scope.wcledata1.length;i++){     
+         var reg=0;
+         var tmp=0;   
+         for(var j=0;j<$scope.wcledata1[i].opt.length;j++){
+            tmp=$scope.wcledata1[i].opt.length*-1;
+             //console.log($scope.wcledata1[i].opt[j].check)
+            reg+=parseInt($scope.wcledata1[i].opt[j].check);
+         }
+         //console.log(tmp)
+        //console.log(reg)
+         if(reg==tmp){
+            return;
+         }
+      }
+      /*多选验证*/
+      
+      for(var i=0;i<$scope.wcledata2.length;i++){     
+         var reg=0;
+         var tmp=0;   
+         for(var j=0;j<$scope.wcledata2[i].opt.length;j++){
+            tmp=$scope.wcledata2[i].opt.length*2;
+             //console.log($scope.wcledata2[i].opt[j].check)
+            reg+=parseInt($scope.wcledata2[i].opt[j].check);
+         }
+         //console.log(tmp)
+        //console.log(reg)
+         if(reg<=tmp){
+            return;
+         }
+      }
+      for(var i=0;i<$scope.wcledata0.length;i++){
+         if($scope.wcledata0[i].oop==''){
+           return;
+         }
+      }
+      for(var i=0;i<$scope.wcledata3.length;i++){
+         if($scope.wcledata3[i].oop==''){
+           return;
+         }
+      }
       /*单选*/
       for(var i=0;i<$scope.wcledata1.length;i++){        
          for(var j=0;j<$scope.wcledata1[i].opt.length;j++){
@@ -81,6 +145,7 @@ angular.module("surveyTimeApp")
          }
          
       }
+      
     /*多选*/ 
       for(var i=0;i<$scope.wcledata2.length;i++){
          for(var j=0;j<$scope.wcledata2[i].opt.length;j++){
@@ -114,6 +179,16 @@ angular.module("surveyTimeApp")
          data:$scope.updata
       }).then(function(e){
          console.log(e)
+         $scope.wcledata0.length=0;
+         $scope.wcledata1.length=0;
+         $scope.wcledata2.length=0;
+         $scope.wcledata3.length=0;
+         if(localStorage.uid){
+            $state.go('cds')
+         }else{
+            $state.go('submit')
+         }
+         
       },function(){})
    }
    
