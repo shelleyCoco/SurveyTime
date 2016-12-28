ed001ee08c79f61e229c96ae5cfcb5036dd935a7
coco <215077830@qq.com>
2016-12-28 20:51:13 +0800

    12.28  富文本

--- app/scripts/controllers/surveyPage.js
+++ app/scripts/controllers/surveyPage.js
@@ -36,3 +36,14 @@ angular.module("surveyTimeApp")
      
    }
 	}])
+ .controller('OptCtrl', function($scope) {
+          $scope.options = {
+            height: 150,
+            toolbar: [
+              ['style', ['bold', 'italic', 'underline', 'clear']],
+              ['color', ['color']],
+              ['para', ['ul', 'ol', 'paragraph']],
+              ['height', ['height']]
+            ]
+          };
+        })
