b006cd63742ef9286ff2228a46179331aae2e3e5
caodongsheng <281135332@qq.com>
2016-12-29 10:48:19 +0800

    vvv

--- app/scripts/controllers/surveyPage.js
+++ app/scripts/controllers/surveyPage.js
@@ -193,3 +193,14 @@ angular.module("surveyTimeApp")
    }
    
 	}])
+.controller('OptCtrl', function($scope) {
+          $scope.options = {
+            height: 150,
+            toolbar: [
+              ['style', ['bold', 'italic', 'underline', 'clear']],
+              ['color', ['color']],
+              ['para', ['ul', 'ol', 'paragraph']],
+              ['height', ['height']]
+            ]
+          };
+        })
